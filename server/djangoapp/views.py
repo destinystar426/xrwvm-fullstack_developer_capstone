c11685311f183e77473a2122753a8ce92517977e
Fiona <dangfiona426@gmail.com>
2025-10-04 15:09:40 -0400

    reviews shown

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -121,8 +121,8 @@ def get_dealer_details(request, dealer_id):
     if dealer_id:
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
-        if isinstance(dealership, list) and len(dealership) > 0:
-            dealership = dealership[0]  # take the first dealer object
+        # if isinstance(dealership, list) and len(dealership) > 0:
+            # dealership = dealership[0]  # take the first dealer object
         return JsonResponse({"status":200,"dealer":dealership})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
